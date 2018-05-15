6ea610cf4da2bdfbfc1efc1d6d63fdd690cd770f
Нестеров Юрий Алексеевич <YANesterov@bellintegrator.com>
2018-05-15 15:25:01 +0400

    add qualifier classes

---
+++ book_store/src/main/java/ru/home/learning/qualifiers/ThirteenDigits.java
@@ -0,0 +1,17 @@
+package ru.home.learning.qualifiers;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Тринадцатизначный квалификатор
+ * Created by YANesterov on 15.05.2018.
+ */
+@Qualifier
+@Retention(RUNTIME)
+@Target({FIELD, TYPE, METHOD})
+public interface ThirteenDigits {
+}
