bb32cb0cfee9e16f6dc5510ee65c4a741c1c3d44
Нестеров Юрий Алексеевич <YANesterov@bellintegrator.com>
2018-05-15 14:44:41 +0400

    add book service and book pojo classes

---
+++ book_store/src/main/java/ru/home/learning/models/Book.java
@@ -0,0 +1,54 @@
+package ru.home.learning.models;
+
+/**
+ * POJO-объект "Книга"
+ * Created by YANesterov on 15.05.2018.
+ */
+public class Book {
+    private String title;
+    private Float price;
+    private String description;
+    private String number;
+
+    public Book() {
+    }
+
+    public Book(String title, Float price, String description, String number) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
--- book_store/src/main/java/ru/home/learning/services/BookService.java
+++ book_store/src/main/java/ru/home/learning/services/BookService.java
@@ -1,9 +1,19 @@
 package ru.home.learning.services;
 
+import ru.home.learning.models.Book;
+
 /**
  * Сервис для для создания книг
  * Created by YANesterov on 15.05.2018.
  */
 @Loggable
 public class BookService {
+    @Inject @ThirteenDigits
+    private NumberGenerator numberGenerator;
+
+    public Book createBook(String title, Float price, String description){
+        Book book = new BookService(title, price, description);
+        book.setNumber(numberGenerator.generateNumber());
+        return book;
+    }
 }
