cabfb195478d9dbf0d4de35723238599c17a7f73
Нестеров Юрий Алексеевич <YANesterov@bellintegrator.com>
2018-05-17 18:29:49 +0400

    - delete logger from integration test

--- book_store/src/test/java/ru/home/learning/services/BookServiceIT.java
+++ book_store/src/test/java/ru/home/learning/services/BookServiceIT.java
@@ -3,11 +3,7 @@ package ru.home.learning.services;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 import org.junit.Test;
-import ru.home.learning.interceptors.bindings.Loggable;
 import ru.home.learning.models.Book;
-
-import javax.inject.Inject;
-import java.util.logging.Logger;
 
 import static org.junit.Assert.assertTrue;
 
@@ -15,15 +11,9 @@ import static org.junit.Assert.assertTrue;
  * Интеграционный тест BookService
  * Created by YANesterov on 17.05.2018.
  */
-@Loggable
 public class BookServiceIT {
-    @Inject
-    private Logger logger;
-
     @Test
     public void shouldCheckNumberIsMOCK() {
-        logger.info("call shouldCheckNumberIsMOCK()");
-
         Weld weld = new Weld();
         WeldContainer container = weld.initialize();
         BookService service = container.instance().select(BookService.class).get();
