4b2e91f125b0b5428497c28c6501ec7783da76ce
Нестеров Юрий Алексеевич <YANesterov@bellintegrator.com>
2018-05-28 11:28:38 +0400

    add email constraint
    - add eamil constraint annotation
    - add resource bundle for constraint messages

---
+++ book_store/src/main/java/ru/home/learning/constraints/Email.java
@@ -0,0 +1,39 @@
+package ru.home.learning.constraints;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Ограничивающая аннотация электронной почты
+ * со встроенной агрегацией ограничений
+ * Created by YANesterov on 24.05.2018.
+ */
+@Size(min = 7)
+@Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\\\." +
+        "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*" +
+        "@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+@ReportAsSingleViolation
+@Constraint(validatedBy = {})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+@Retention(RUNTIME)
+public @interface Email {
+    String message() default "{ru.home.learning.constraints.Email.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+    @Retention(RUNTIME)
+    @interface List {
+        Email[] value();
+    }
+}
