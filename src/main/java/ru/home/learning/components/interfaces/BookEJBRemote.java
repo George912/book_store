a8d0934bfc8cfc9eb99cefa24a6e1cdabf278c67
Yuri Nesterov <nesteroviury@yandex.ru>
2018-07-03 11:46:22 +0400

    definition data source
    - add database populator
    - add javadocs to BookEJBRemote
    - modify JNDI name into persistence.xml

--- src/main/java/ru/home/learning/components/interfaces/BookEJBRemote.java
+++ src/main/java/ru/home/learning/components/interfaces/BookEJBRemote.java
@@ -10,11 +10,33 @@ import java.util.List;
  */
 @Remote
 public interface BookEJBRemote {
+    /**
+     * Использует именованный запрос findAllBooks, определенный
+     * в сущности Book, чтобы получить все экземпляры книг из базы данных
+     *
+     * @return
+     */
     List<Book> findBooks();
 
+    /**
+     * Принимает объект типа Book (которая не может иметь значение null)
+     * в качестве параметра, сохраняет его в базе данных и затем возвращает его
+     *
+     * @param book объект типа Book
+     * @return объект типа Book
+     */
     Book createBook(Book book);
 
     void deleteBook(Book book);
 
+    /**
+     * Принимает отдельные объекты типа Book (которые не могут иметь
+     * значение null) в качестве параметра и объединяет их.
+     * С помощью метода merge() объект прикрепляется к менеджеру сущностей
+     * и синхронизируется с базой данных
+     *
+     * @param book объект типа Book
+     * @return объект типа Book
+     */
     Book updateBook(Book book);
 }
