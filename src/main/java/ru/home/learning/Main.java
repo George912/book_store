acc132120ba1763c04043886097d44f84308115b
Yuri Nesterov <nesteroviury@yandex.ru>
2018-07-03 16:03:37 +0400

    modifying Main for use EJB

--- src/main/java/ru/home/learning/Main.java
+++ src/main/java/ru/home/learning/Main.java
@@ -1,26 +1,35 @@
 package ru.home.learning;
 
+import ru.home.learning.components.BookEJB;
 import ru.home.learning.models.Book;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.List;
 
 /**
- * Точка входа
+ * Класс Main, вызывающий BookEJBRemote
  * Created by YANesterov on 15.05.2018.
  */
 public class Main {
-    public static void main(String[] args) {
-        Book book = new Book("H2G2", 12.5f, "Learn Java EE 7", "1-84023-742-2", 354, false);
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("demoPU");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        em.persist(book);
-        tx.commit();
-        em.close();
-        emf.close();
+    public static void main(String[] args) throws NamingException {
+        Context context = new InitialContext();
+        BookEJB bookEJB = (BookEJB) context.lookup("java:global/book_store-1.0-SNAPSHOT/BookEJB!" +
+                "ru.home.learning.components.interfaces.BookEJBRemote");
+
+        List<Book> books = bookEJB.findBooks();
+        for (Book book : books) {
+            System.out.println("--- " + book);
+        }
+        Book book = new Book("The Hitchhiker's Guide to the Galaxy", 12.5F
+                , "Science fiction by Douglas Adams.", "1-24561-799-0", 354, false);
+        book = bookEJB.createBook(book);
+        System.out.println("### Book created : " + book);
+        book.setTitle("H2G2");
+        book = bookEJB.updateBook(book);
+        System.out.println("### Book updated : " + book);
+        bookEJB.deleteBook(book);
+        System.out.println("### Book deleted");
     }
 }
